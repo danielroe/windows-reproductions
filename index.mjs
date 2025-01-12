00bd1c027e6ea4bbcfc03287b4587a03936b14c0
Daniel Roe <daniel@roe.dev>
2025-01-12 12:03:22 +0000

    chore: add back pathe to show it's not a node:path issue

--- index.mjs
+++ index.mjs
@@ -1,16 +1,30 @@
-import { dirname, resolve } from 'node:path'
+import { dirname, resolve as nodeResolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
+
+import { resolve as patheResolve } from 'pathe'
 import { resolvePathSync } from 'mlly'
 
 console.log({
   'import.meta.url': import.meta.url,
   'fileURLToPath(import.meta.url)': fileURLToPath(import.meta.url),
-  "resolve(dirname(fileURLToPath(import.meta.url)), 'app/plugins/router')": resolve(dirname(fileURLToPath(import.meta.url)), 'app/plugins/router')
+  "nodeResolve(dirname(fileURLToPath(import.meta.url)), 'app/plugins/router')": nodeResolve(dirname(fileURLToPath(import.meta.url)), 'app/plugins/router'),
+  "patheResolve(dirname(fileURLToPath(import.meta.url)), 'app/plugins/router')": patheResolve(dirname(fileURLToPath(import.meta.url)), 'app/plugins/router')
 })
 
-console.log(
-  resolvePathSync(
-    resolve(dirname(fileURLToPath(import.meta.url)), 'app/plugins/router'),
-    { extensions: ['.ts', '.js'] }
-  )
-)
+const resolves = { nodeResolve, patheResolve }
+
+for (const r in resolves) {
+  console.log('trying', r)
+
+  try {
+    console.log(
+      resolvePathSync(
+        resolves[r](dirname(fileURLToPath(import.meta.url)), 'app/plugins/router'),
+        { extensions: ['.ts', '.js'] }
+      )
+    )
+  } catch (e) {
+    console.log('failed with', r)
+    console.error(e)
+  }
+}
