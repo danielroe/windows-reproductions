3cf73bcece086d6fd758ae26ed237b88b73506c2
Daniel Roe <daniel@roe.dev>
2025-01-12 11:35:05 +0000

    chore: add more logging

--- dist/index.mjs
+++ dist/index.mjs
@@ -2,6 +2,10 @@ import { resolvePathSync } from 'mlly'
 import { distDir } from './chunks/dirs.mjs'
 import { resolve } from 'pathe'
 
+console.log('distDir', distDir)
+
+console.log('resolving', resolve(distDir, 'app/plugins/router'))
+
 console.log(
   resolvePathSync(resolve(distDir, 'app/plugins/router'), { extensions: ['.ts', '.js'] })
 )
