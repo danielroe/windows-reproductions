7ea951e7139b03f63cca1e0d8bfbde2af0c80c36
Daniel Roe <daniel@roe.dev>
2025-01-12 11:43:16 +0000

    chore: remove pathe to simplify repro

--- index.mjs
+++ index.mjs
@@ -1,10 +1,10 @@
+import { dirname, resolve } from 'node:path'
 import { resolvePathSync } from 'mlly'
-import { resolve } from 'pathe'
 import { fileURLToPath } from 'mlly'
 
 console.log('url', import.meta.url)
 console.log('path', fileURLToPath(import.meta.url))
 
 console.log(
-  resolvePathSync(resolve(fileURLToPath(import.meta.url), 'app/plugins/router'), { extensions: ['.ts', '.js'] })
+  resolvePathSync(resolve(dirname(fileURLToPath(import.meta.url)), 'app/plugins/router'), { extensions: ['.ts', '.js'] })
 )
