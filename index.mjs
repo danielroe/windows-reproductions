8e65aea53f4896c6f4150adb987297be18774a0f
Daniel Roe <daniel@roe.dev>
2025-01-12 11:59:02 +0000

    chore: improve logging

--- index.mjs
+++ index.mjs
@@ -1,10 +1,16 @@
 import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { resolvePathSync } from 'mlly'
-import { fileURLToPath } from 'mlly'
 
-console.log('url', import.meta.url)
-console.log('path', fileURLToPath(import.meta.url))
+console.log({
+  'import.meta.url': import.meta.url,
+  'fileURLToPath(import.meta.url)': fileURLToPath(import.meta.url),
+  "resolve(dirname(fileURLToPath(import.meta.url)), 'app/plugins/router')": resolve(dirname(fileURLToPath(import.meta.url)), 'app/plugins/router')
+})
 
 console.log(
-  resolvePathSync(resolve(dirname(fileURLToPath(import.meta.url)), 'app/plugins/router'), { extensions: ['.ts', '.js'] })
+  resolvePathSync(
+    resolve(dirname(fileURLToPath(import.meta.url)), 'app/plugins/router'),
+    { extensions: ['.ts', '.js'] }
+  )
 )
